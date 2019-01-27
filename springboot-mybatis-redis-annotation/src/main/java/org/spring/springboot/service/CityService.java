614b3576e93c4c9412bc35277cbd23eabe7d1cea
1303575952@qq.com <1303575952@qq.com>
2019-01-27 10:31:52 +0800

    springboot-mybatis-redis-annotation代码缺失

---
+++ springboot-mybatis-redis-annotation/src/main/java/org/spring/springboot/service/CityService.java
@@ -0,0 +1,30 @@
+package org.spring.springboot.service;
+
+import org.spring.springboot.domain.City;
+
+import java.util.List;
+
+/**
+ * 城市业务逻辑接口类
+ */
+public interface CityService {
+
+    /**
+     * 获取城市
+     *
+     */
+    City getCityByName(String cityName);
+
+    /**
+     * 新增城市信息
+     *
+     */
+    void saveCity(City city);
+
+    /**
+     * 更新城市信息
+     *
+     */
+    void updateCityDescription(String cityName, String description);
+
+}
